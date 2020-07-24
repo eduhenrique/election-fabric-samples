04ce5000c812f21c32d0eed18245f34c612c961e
eduardo henrique <eduhenrique.a@gmail.com>
2020-07-24 01:18:39 -0300

    change voto model

--- chaincode/processo_eleitoral/typescript/src/models/voto.ts
+++ chaincode/processo_eleitoral/typescript/src/models/voto.ts
@@ -2,7 +2,7 @@ import { Candidato } from "./candidato";
 
 export class Voto {
     public docType?: string;    
-    // public eleitorHash: string; // a key será o hash?
+    public eleitorHash: string; // a key será o hash?
     public candidatoNum: string;
     public candidato?: Candidato;
 }
