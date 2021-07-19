70e78228c84b2f4bdd24f69d3fefcc826da5c34f
eduardo henrique <eduhenrique.a@gmail.com>
2021-07-19 02:39:00 -0300

    Separating the enrollAdminUser in another .sh script

---
+++ electoral_process/typescript/enrollAdminUser.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+node dist/enrollAdmin && node dist/user_staff/registerStaffUser 
+
--- electoral_process/typescript/simulateClient.sh
+++ electoral_process/typescript/simulateClient.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-node dist/enrollAdmin && node dist/user_staff/registerStaffUser 
+./enrollAdminUser.sh 
 node dist/createPosition && node dist/user_staff/requestInitiateParticipant && ./submitCandidates.sh
